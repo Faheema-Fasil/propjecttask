80b0aef5fd2b3da19393c7c8d05376384cdb79a7
Faheema Fasil <faheemafasil12@gmail.com>
2025-02-13 13:06:45 +0400

    Add

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useAuth } from "../firebase";
 import { useNavigate } from "react-router-dom";
 import logoutIcon from "../assets/logout_icon.png";
@@ -16,6 +16,24 @@ const Header: React.FC = () => {
       console.error(error);
     }
   };
+
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        dropdownRef.current.classList.add("hidden");
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
 
   return (
     <div>
@@ -45,15 +63,32 @@ const Header: React.FC = () => {
         </div>
       </div>
 
-      <div className="flex md:hidden justify-between items-center bg-purple-100 p-3 border-b border-gray-300">
+      <div className="flex md:hidden justify-between items-center bg-purple-100 p-4 border-b border-gray-300 relative z-[99]">
         <div>TaskBuddy</div>
-        <div>
-          {" "}
+        <div className="relative">
           <img
-            className="rounded-full bg-gray-100 w-8 h-8 mr-2"
+            className="rounded-full bg-gray-100 w-8 h-8 mr-2 cursor-pointer"
             src={currentUser?.photoURL}
             alt=""
+            onClick={() => {
+              const dropdown = dropdownRef.current;
+              if (dropdown) {
+                dropdown.classList.toggle("hidden");
+              }
+            }}
           />
+          <div
+            ref={dropdownRef}
+            className="hidden absolute right-0 mt-2 bg-white border border-gray-300 rounded-lg p-2 z-10"
+          >
+            <button
+              onClick={handleLogout}
+              className="border px-4 bg-purple-100 mt-2 border-purple-300 rounded-lg p-1 flex items-center justify-center hover:bg-purple-200 transition duration-200 w-full"
+            >
+              <img src={logoutIcon} alt="" />
+              <span className="ml-2 text-sm">Logout</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -61,3 +96,4 @@ const Header: React.FC = () => {
 };
 
 export default Header;
+
